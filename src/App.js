92a22b665b5c6d2cb51a53c38a994dff0df314e4
Andy <andrew.sunglae.kim@gmail.com>
2016-09-07 11:23:15 -0400

    can get forecasts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Forecast from './components/Forecast'
+import ForecastForm from './components/ForecastForm'
 import ForecastModel from './models/forecast'
 
 class App extends Component {
@@ -21,10 +22,20 @@ class App extends Component {
     }.bind(this))
     console.log("bob");
   }
+  getForecast(location){
+    let forecast = new ForecastModel(location.city, location.state)
+    forecast.getForecast().then(function(res){
+      this.setState({
+        forecast: forecast
+      })
+    }.bind(this))
+  }
   render() {
     return (
       <div className="App">
         <h1>Weathereact</h1>
+        <ForecastForm
+          getForecast={this.getForecast.bind(this)}/>
         <Forecast
           forecast={this.state.forecast}
           />
--- src/components/ForecastForm.js
+++ src/components/ForecastForm.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react'
 
 class ForecastForm extends Component {
+  constructor(){
+    super()
+    this.state = {
+      city: "",
+      state: ""
+    }
+  }
+  onCityChange(event){
+    this.setState({
+      city: event.target.value
+    })
+  }
+  onStateChange(event){
+    this.setState({
+      state: event.target.value
+    })
+  }
+  submitForm(event){
+    event.preventDefault()
+    this.props.getForecast(this.state)
+    this.setState({
+      city: '',
+      state: ''
+    })
+  }
   render(){
     return(
-      <form>
+      <form onSubmit={e => this.submitForm(e)}>
         <label>City:</label>
-        <input type="text">
-        <label>City:</label>
-        <input type="text">
+        <input
+          type="text"
+          onChange={e => this.onCityChange(e)}
+          placeholder="City"
+          value={this.state.city || ''}/>
+        <label>State:</label>
+        <input
+          type="text"
+          onChange={e => this.onStateChange(e)}
+          placeholder="State"
+          value={this.state.state || ''}/>
+        <button type='submit'>Get Forecast</button>
       </form>
     )
   }
